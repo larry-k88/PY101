163a4435f129b80424e0ede4ce6c77598fd9687d
Larry Knight <larryknight88@gmail.com>
2024-12-10 16:18:31 +0000

    lesson 2 notes and projects updated after assignment 13

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -14,32 +14,59 @@
 # START
 # GET number_1 and number_2
 # SET number_1 and number_2
-# GET operation 
+# GET operation
 # IF operation = 1 of 4 options:
 #   Perform that operation
 # PRINT the result
 # END
 
-print('Welcome to Calculator: choose two numbers and an operation to perform!')
+def prompt(message):
+    print(f'==> {message}')
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator: '
+       'choose two numbers and an operation to perform!')
 
 # Ask user for two numbers
-print('What is the first number?')
-number_1 = int(input())
-print('What is the second number?')
-number_2 = int(input())
+prompt('What is the first number?')
+number_1 = input()
 
-print(f'You chose {number_1} and {number_2}')
+while invalid_number(number_1):
+    prompt("That doesn't look like a valid number.")
+    number_1 = input()
 
-print('What operation would you like to perform?\n1) Add 2) Subtract '
+prompt('What is the second number?')
+number_2 = input()
+
+while invalid_number(number_2):
+    prompt("That doesn't look like a valid number.")
+    number_2 = input()
+
+prompt(f'You chose {number_1} and {number_2}')
+
+prompt('What operation would you like to perform?\n1) Add 2) Subtract '
       '3) Multiply 4) Divide')
 operation = input()
 
-if operation == '1':
-    output = number_1 + number_2
-elif operation == '2':
-    output = number_1 - number_2
-elif operation == '3':
-    output = number_1 * number_2
-elif operation == '4':
-    output = number_1 / number_2
-print(f'The result is {output}')
+while operation not in ['1', '2', '3', '4']:
+    prompt("You must choose 1, 2, 3 or 4")
+    operation = input()
+
+match operation:
+    case '1':
+        output = int(number_1) + int(number_2)
+    case '2':
+        output = int(number_1) - int(number_2)
+    case '3':
+        output = int(number_1) * int(number_2)
+    case '4':
+        output = int(number_1) / int(number_2)
+
+prompt(f'The result is {output}')
