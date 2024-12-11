5a8bdbb3504a5c47adcd54defc0cd8d4daca6d45
Larry Knight <larryknight88@gmail.com>
2024-12-11 16:00:13 +0000

    notes and files updated after assignment 15

---
+++ lesson_2/debug.py
@@ -0,0 +1,17 @@
+# debug.py
+
+'''counter = 1
+
+while counter <= 5:
+    print(counter)
+    breakpoint()
+    counter += 1 
+'''
+
+def my_function():
+    x = 5
+    breakpoint()
+    y = x + 1
+    print(y)
+
+my_function()
--- test.py
+++ test.py
@@ -1,29 +1,13 @@
-def process_student(student_data):
-    name = student_data.get('name')
-    grade = student_data.get('grade')
-    return (name, grade)
+def titlize(sentence):
+    words = sentence.split()
+    new_words = []
 
-def average_grade(grades):
-    total = sum(grades)
-    average = total / len(grades)
-    return average
+    for word in words:
+        if len(word) > 2:
+            word = word.capitalize()
+        new_words.append(word)
 
-students = [
-    {'name': 'Alice', 'grade': 85},
-    {'name': 'Bob'},
-    {'name': 'Jack', 'grade': 72},
-    {'name': 'Jane', 'grade': 75},
-]
+    return ' '.join(new_words)
 
-def collect_grades(students):
-    grades = []
-    for student in students:
-        name, grade = process_student(student)
-        if grade != None:
-            grades.append(grade)
-    return grades
-
-grades = collect_grades(students)
-print(average_grade(grades))
-# TypeError: unsupported operand type(s) for +: 'int'
-# and 'NoneType'
+title = 'hello world of programming'
+print(titlize(title))
