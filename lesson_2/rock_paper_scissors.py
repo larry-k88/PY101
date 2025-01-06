c42bfafb4feea3fda575622d8151d75f2b471fea
Larry Knight <larryknight88@gmail.com>
2025-01-06 16:33:38 +0000

    Assignment 26 Q3 (no break statement) added for RPS

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -42,3 +42,30 @@ while True:
             prompt('Please enter y or n...')
     if play_again[0] == 'n':
         break
+
+'''
+Lines 23-44 can be replaced by the below, removing the need to have a
+break statement (as per Q3 on Assignment 26)
+
+keep_running = True
+while keep_running:
+    prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
+    choice = input()
+
+    while choice not in VALID_CHOICES:
+        prompt("That's not a valid choice")
+        choice = input()
+
+    computer_choice = random.choice(VALID_CHOICES)
+
+    prompt(display_winner(choice, computer_choice))
+
+    prompt('Do you want to play again? y/n')
+    play_again = input().lower()
+    while play_again not in ['y', 'n']:
+        prompt('Please enter y or n...')
+        play_again = input().lower()
+
+    if play_again == 'n':
+        keep_running = False
+'''
