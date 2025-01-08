457b8b65fe34ce28e28c7b6b9aec030131055a2d
Larry Knight <larryknight88@gmail.com>
2025-01-08 10:29:28 +0000

    bonus 2 -shortened input completed for rps

--- lesson_2/rps_bonus.py
+++ lesson_2/rps_bonus.py
@@ -1,89 +1,81 @@
 import random
 import os
 
-VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+OPTIONS_DICT = {
+                'r' : 'rock',
+                'p' : 'paper',
+                's' : 'scissors',
+                'l' : 'lizard',
+                'sp' : 'spock',
+            }
+
+OPTIONS_ABBREVIATED = list(OPTIONS_DICT.keys())
+
+WINNING_OUTCOMES = {
+                'r' : ['s', 'l'],
+                'p' : ['r', 'sp'],
+                's' : ['p', 'l'],
+                'l' : ['p', 'sp'],
+                'sp' : ['r', 's'],
+                }
 
 os.system('clear')
+
 def prompt(message):
     print(f'==> {message}')
 
-def display_winner(player, computer):
-    prompt(f'You chose: {player}, computer chose: {computer}')
-    if (
-        (player == 'rock' and computer == 'scissors') or
-        (player == 'rock' and computer == 'lizard') or
-        (player == 'paper' and computer == 'rock') or
-        (player == 'paper' and computer == 'spock') or
-        (player == 'scissors' and computer == 'paper') or
-        (player == 'scissors' and computer == 'lizard') or
-        (player == 'lizard' and computer == 'spock') or
-        (player == 'lizard' and computer == 'paper') or
-        (player == 'spock' and computer == 'scissors') or
-        (player == 'spock' and computer == 'rock')
-    ):
-        return 'You win!'
-    elif (
-            (player == 'rock' and computer == 'paper') or
-            (player == 'rock' and computer == 'spock') or
-            (player == 'paper' and computer == 'scissors') or
-            (player == 'paper' and computer == 'lizard') or
-            (player == 'scissors' and computer == 'rock') or
-            (player == 'scissors' and computer == 'spock') or
-            (player == 'lizard' and computer == 'rock') or
-            (player == 'lizard' and computer == 'scissors') or
-            (player == 'spock' and computer == 'paper') or
-            (player == 'spock' and computer == 'lizard')
-    ):
-        return 'Computer wins!'
-    else:
-        return 'It\'s a tie!'
+def display_welcome():
+    prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
 
+def display_options():
+    for k, v in OPTIONS_DICT.items():
+        print(f'{k} for {v}')
+
+def decide_winner(player, computer):
+    prompt(f'You chose: {OPTIONS_DICT.get(player)}, '
+           f'computer chose: {OPTIONS_DICT.get(computer)}')
+    if computer in WINNING_OUTCOMES[player]:
+        return 'player_wins'
+    if player in WINNING_OUTCOMES[computer]:
+        return 'computer_wins'
+    return 'tie'
+
+def display_winner(winner):
+    match winner:
+        case 'player_wins':
+            prompt('You win!')
+        case 'computer_wins':
+            prompt('Computer wins!')
+        case 'tie':
+            prompt('It\'s a tie!')
+
+display_welcome()
 while True:
-    prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
-    choice = input()
+    prompt('To make your choice, type one of the following:')
+    display_options()
+    user_choice = input('Select an option: ').casefold()
 
-    while choice not in VALID_CHOICES:
+    while user_choice not in OPTIONS_ABBREVIATED:
         prompt("That's not a valid choice")
-        choice = input()
+        user_choice = input('Select an option: ').casefold()
 
-    computer_choice = random.choice(VALID_CHOICES)
+    computer_choice = random.choice(OPTIONS_ABBREVIATED)
 
-    prompt(display_winner(choice, computer_choice))
+    who_wins = decide_winner(user_choice, computer_choice)
+
+    display_winner(who_wins)
 
     while True:
         prompt('Do you want to play again? y/n')
-        play_again = input().lower()
+        play_again = input().casefold()
 
-        if play_again in ['y', 'n']:
-            break
-        else:
+        if play_again not in ['y', 'n']:
             prompt('Please enter y or n...')
-    if play_again[0] == 'n':
-        break
+            play_again = input().casefold()
 
-'''
-Lines 23-44 can be replaced by the below, removing the need to have a
-break statement (as per Q3 on Assignment 26)
-
-keep_running = True
-while keep_running:
-    prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
-    choice = input()
-
-    while choice not in VALID_CHOICES:
-        prompt("That's not a valid choice")
-        choice = input()
-
-    computer_choice = random.choice(VALID_CHOICES)
-
-    prompt(display_winner(choice, computer_choice))
-
-    prompt('Do you want to play again? y/n')
-    play_again = input().lower()
-    while play_again not in ['y', 'n']:
-        prompt('Please enter y or n...')
-        play_again = input().lower()
+        else:
+            break
 
     if play_again == 'n':
-        keep_running = False
-'''
+        break
+    os.system('clear')
